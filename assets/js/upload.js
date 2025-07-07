ae13c60fff6dc615260a6e43a01f14ae2fca2bf6
Monika-Viktor <viktor.studenyak@maxplanckschools.de>
2025-07-07 11:54:06 +0200

    updated link

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -11,7 +11,7 @@
  * Done!  The static site will now POST images directly to Google Drive.
  */
 
-const scriptURL = "https://script.google.com/macros/s/AKfycbxYXRExqp9uvJY3Ogs46vRMNeJv9V9AHthappPt1p_tllGlkH9Y9787TiJkF92TlQOy/exec"; // ← replace
+const scriptURL = "https://script.google.com/macros/s/AKfycbwLpSju8OqfpD8gYcWF-Ew-PC4OO66eQqLU2lb-aC-GgCt1UTmIopvV1XTQdtY2ydef/exec"; // ← replace
 
 const uploadForm = document.getElementById("uploadForm");
 const photoInput = document.getElementById("photoFiles");
